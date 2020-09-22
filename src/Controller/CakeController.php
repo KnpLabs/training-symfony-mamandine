3c5f547ebae8e7489e9aded8f0643231d89fbfe9
Lenaic <lcouellan@gmail.com>
2020-09-22 23:07:24 +0200

    Get env variable

--- src/Controller/CakeController.php
+++ src/Controller/CakeController.php
@@ -8,12 +8,14 @@ class CakeController extends AbstractController
 {
     public function list()
     {
-        $pdo = new \PDO('sqlite:'. __DIR__ .'/../../var/cakes.sqlite');
+        $pdo = new \PDO($this->getParameter('pdo_dsn'));
 
         $cakes = $pdo
             ->query('SELECT * FROM cake ORDER BY created_at DESC')
             ->fetchAll()
         ;
+
+        dump($this->getParameter('environment'));
 
         return $this->render('cake/list.html.twig', [
             'cakes' => $cakes,
