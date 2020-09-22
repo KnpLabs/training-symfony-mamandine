d0d6d80d9498efe2a38710cc73e593f2373b4869
Lenaic <lcouellan@gmail.com>
2020-09-22 22:01:36 +0200

    Initial commit

---
+++ src/Controller/CakeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CakeController extends AbstractController
+{
+    public function list()
+    {
+        $pdo = new \PDO('sqlite:'. __DIR__ .'/../../var/cakes.sqlite');
+
+        $cakes = $pdo
+            ->query('SELECT * FROM cake ORDER BY created_at DESC')
+            ->fetchAll()
+        ;
+
+        return $this->render('cake/list.html.twig', [
+            'cakes' => $cakes,
+        ]);
+    }
+}
