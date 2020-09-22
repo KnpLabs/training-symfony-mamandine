34b79959000b19aecc67780e197516a50bc7caa5
Lenaic <lcouellan@gmail.com>
2020-09-22 23:28:03 +0200

    Refacto to handle common layout + Show a cake

--- src/Controller/CakeController.php
+++ src/Controller/CakeController.php
@@ -6,19 +6,57 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CakeController extends AbstractController
 {
+    private $pdo;
+
+
     public function list()
     {
-        $pdo = new \PDO($this->getParameter('pdo_dsn'));
-
-        $cakes = $pdo
+        $cakes = $this
+            ->getPdo()
             ->query('SELECT * FROM cake ORDER BY created_at DESC')
             ->fetchAll()
         ;
 
-        dump($this->getParameter('environment'));
-
         return $this->render('cake/list.html.twig', [
             'cakes' => $cakes,
         ]);
+    }
+
+    public function show($cakeId)
+    {
+        $cake = $this
+            ->getPdo()
+            ->query(sprintf('SELECT * FROM cake WHERE id = %d', $cakeId))
+            ->fetch()
+        ;
+
+
+        if (!$cake) {
+            throw $this->createNotFoundException(sprintf('The cake with id "%s" was not found.', $cakeId));
+        }
+
+        $categories = $this
+            ->getPdo()
+            ->query(sprintf('
+                SELECT * FROM category
+                INNER JOIN cake_category ON category.id = cake_category.category_id
+                WHERE cake_category.cake_id = %d
+            ', $cakeId))
+            ->fetchAll()
+        ;
+
+        return $this->render('cake/show.html.twig', [
+            'cake' => $cake,
+            'categories' => $categories,
+        ]);
+    }
+
+    private function getPdo()
+    {
+        if (null === $this->pdo) {
+            $this->pdo = new \PDO($this->getParameter('pdo_dsn'));
+        }
+
+        return $this->pdo;
     }
 }
