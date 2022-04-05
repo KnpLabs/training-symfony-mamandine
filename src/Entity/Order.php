ad6532792677516498e0db2d2363a404bef8b843
Lenaic <lcouellan@gmail.com>
2022-04-05 09:18:07 +0200

    feat: implement workflow component

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -45,6 +45,9 @@ class Order
      */
     private $number;
 
+    // the configured marking store property must be declared
+    private $currentPlace;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -109,4 +112,15 @@ class Order
 
         return $this;
     }
+
+     // getter/setter methods must exist for property access by the marking store
+     public function getCurrentPlace()
+     {
+         return $this->currentPlace;
+     }
+
+     public function setCurrentPlace($currentPlace, $context = [])
+     {
+         $this->currentPlace = $currentPlace;
+     }
 }
